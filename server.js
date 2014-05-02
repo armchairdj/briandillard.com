22872e0bd2c78cfdbbfd00e3984a2b40786c6991
Brian J. Dillard <armchairdj@gmail.com>
2014-05-02 18:00:26 -0500

    toward express and ejs

--- server.js
+++
@@ -1,9 +0,0 @@
-/* Module dependencies */
-var express = require('express');
-
-/* App bootstrapping */
-var app = express();
-
-var server = app.listen(1229, function() {
-  console.log('Listening on port %d', server.address().port);
-});
